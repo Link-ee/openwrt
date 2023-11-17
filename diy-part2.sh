ff50a55d1a8f0855014e6e56119aafa142898e0d
OwO-GitHub <79355200+OwO-GitHub@users.noreply.github.com>
2023-11-17 10:14:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,5 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Set etc/openwrt_release
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/themes/luci-theme-argon
