aadb4396110ac802b7241a7712805adfe41c0308
Link <79355200+Link-ee@users.noreply.github.com>
2024-01-19 00:03:39 +0800

    Update diy-part2.sh

--- im-21.02/diy-part2.sh
+++ im-21.02/diy-part2.sh
@@ -1,2 +1,6 @@
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+rm -rf feeds/luci/applications/luci-app-turboacc
+git clone https://github.com/coolsnowwolf/luci.git lede
+cp -rf lede/applications/luci-app-turboacc package/turboacc
+rm -rf lede
