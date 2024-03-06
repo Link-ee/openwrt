57c179cc0b130670c6f0b9bca8d62f5fb1c860a4
Link <79355200+Link-ee@users.noreply.github.com>
2024-03-06 09:25:36 +0800

    Update diy-part2.sh

--- lede/diy-part2.sh
+++ lede/diy-part2.sh
@@ -14,9 +14,3 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Set etc/openwrt_release
-
-rm -rf feeds/luci/applications/luci-app-argon-config
-rm -rf feeds/luci/themes/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-#rm -rf feeds/luci/applications/luci-app-ssr-plus
